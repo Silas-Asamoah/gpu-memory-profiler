0524c018969601fec63d5d4be3ea0a957035a17a
Silas-Asamoah <silasbempong@gmail.com>
2023-08-26 14:42:19 +0000

    style formatting

--- tensor_torch_profiler/profiler.py
+++ tensor_torch_profiler/profiler.py
@@ -1,7 +1,13 @@
+import logging
 import time
-import torch
-import tensorflow as tf
 from collections import defaultdict
+
+import tensorflow as tf
+import torch
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
 
 class TensorTorchProfiler:
     def __init__(self):
@@ -10,19 +16,19 @@ class TensorTorchProfiler:
     def profile(self, func, *args, **kwargs):
         start_time = time.time()
         start_memory = torch.cuda.memory_allocated()
-        
+
         result = func(*args, **kwargs)
-        
+
         end_time = time.time()
         end_memory = torch.cuda.memory_allocated()
-        
+
         self.stats[func.__name__]["time"] += end_time - start_time
         self.stats[func.__name__]["memory"] += end_memory - start_memory
-        
+
         return result
 
     def report(self):
         for name, stat in self.stats.items():
-            print(f"Function: {name}")
-            print(f"Total execution time: {stat['time']} seconds")
-            print(f"Total memory usage: {stat['memory']} bytes")
+            logger.info(f"Function: {name}")
+            logger.info(f"Total execution time: {stat['time']} seconds")
+            logger.info(f"Total memory usage: {stat['memory']} bytes")
