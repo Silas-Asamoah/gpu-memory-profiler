d1f2bd7e5adbafeadbb784bfd221d242f8515c0f
Silas-Asamoah <silasbempong@gmail.com>
2023-08-26 15:10:58 +0000

    feat: profile tensorflow execution

--- tensor_torch_profiler/profiler.py
+++ tensor_torch_profiler/profiler.py
@@ -10,10 +10,20 @@ logger.setLevel(logging.INFO)
 
 
 class TensorTorchProfiler:
+    """
+    A profiler for TensorFlow and PyTorch.
+    """
     def __init__(self):
         self.stats = defaultdict(lambda: {"time": 0, "memory": 0})
 
     def profile(self, func, *args, **kwargs):
+        """
+        Profile a function.
+        :param func: The function to profile.
+        :param args: The arguments to pass to the function.
+        :param kwargs: The keyword arguments to pass to the function.
+        :return: The result of the function.
+        """
         start_time = time.time()
         start_memory = torch.cuda.memory_allocated()
 
@@ -26,8 +36,28 @@ class TensorTorchProfiler:
         self.stats[func.__name__]["memory"] += end_memory - start_memory
 
         return result
+    
+    def profile_tensorflow(self, func, *args, **kwargs):
+        """
+        Profile a tensorflow function.
+        :param func: The tensorflow function to profile.
+        :param args: The arguments to pass to the function.
+        :param kwargs: The keyword arguments to pass to the function.
+        :return: The result of the function.
+        """
+        tf.profiler.experimental.start('logdir')
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        tf.profiler.experimental.stop()
+
+        self.stats[func.__name__]["time"] += end_time - start_time
+        return result
 
     def report(self):
+        """
+        Report the profiling results.
+        """
         for name, stat in self.stats.items():
             logger.info(f"Function: {name}")
             logger.info(f"Total execution time: {stat['time']} seconds")
