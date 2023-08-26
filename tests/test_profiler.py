b033046dd42920d00a93a6028591ab726559a47e
Silas-Asamoah <silasbempong@gmail.com>
2023-08-26 15:14:16 +0000

    test: profile tensorflow

--- tests/test_profiler.py
+++ tests/test_profiler.py
@@ -1,5 +1,6 @@
 import pytest
 from tensor_torch_profiler.profiler import TensorTorchProfiler
+import tensorflow as tf
 
 def test_profiler():
     profiler = TensorTorchProfiler()
@@ -12,5 +13,19 @@ def test_profiler():
     assert "dummy_func" in profiler.stats
     assert profiler.stats["dummy_func"]["time"] >= 0
     assert profiler.stats["dummy_func"]["memory"] >= 0
+
+    profiler.report()
+
+def test_profile_tensorflow():
+    profiler = TensorTorchProfiler()
+
+    def dummy_tensorflow_func():
+        x = tf.constant([1, 2, 3, 4, 5])
+        return tf.reduce_sum(x)
+
+    profiler.profile_tensorflow(dummy_tensorflow_func)
+
+    assert "dummy_tensorflow_func" in profiler.stats
+    assert profiler.stats["dummy_tensorflow_func"]["time"] >= 0
 
     profiler.report()
